ac6a225f283a814ff89de6031e00833eb3336922
Andrei Prokofjev <andrei.prokofjev@gmail.com>
2017-05-18 09:10:57 +0300

    Iterator pattern

---
+++ src/com/apro/patterns/behavioral/iterator/Container.java
@@ -0,0 +1,5 @@
+package com.apro.patterns.behavioral.iterator;
+
+public interface Container {
+    Iterator getIterator();
+}
---
+++ src/com/apro/patterns/behavioral/iterator/Iterator.java
@@ -0,0 +1,7 @@
+package com.apro.patterns.behavioral.iterator;
+
+public interface Iterator {
+    boolean hasNext();
+
+    Object next();
+}
